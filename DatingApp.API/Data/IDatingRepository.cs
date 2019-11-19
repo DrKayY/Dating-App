6fa70e77cd2bc7a031252c035bf10e1fd25435dc
Yusuff Bello <ykbello2011@gmail.com>
2019-11-19 12:40:43 +0100

    End of Section 11

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -11,6 +11,8 @@ namespace DatingApp.API.Data
          Task<bool> SaveAll();
          Task<IEnumerable<User>> GetUsers();
          Task<User> GetUser(int Id);
+         Task<Photo> GetPhoto(int id);
 
+         Task<Photo> GetMainPhotoForUser(int userId);
     }
 }
